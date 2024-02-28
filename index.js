3998c9afef9a17f237b734795f7c1fa1937ea2ab
Akash-Cogg <akash@cognitivegarage.ai>
2024-02-28 16:10:49 +0530

    temp commit

---
+++ index.js
@@ -0,0 +1,13 @@
+ function printName1(name) {
+console.log("name is:",name)
+    
+}
+function printName() {
+    console.log("name is: Akash Gupta")   
+}
+
+function printName3(name,returnName) {
+console.log("name is:",name)
+returnName = name+"GUPTA"
+return(returnName)
+}
