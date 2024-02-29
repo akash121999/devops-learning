4f0b19023a729f6d1c9e9b1c09855efc616260ad
akash121999 <akashgupta520210@gmail.com>
2024-02-29 12:29:09 +0530

    temp commit

---
+++ index.js
@@ -0,0 +1,13 @@
+export const printName1=(name)=> {
+    console.log("name is:",name)
+        
+    }
+    export const printName= ()=> {
+        console.log("name is: Akash Gupta")   
+    }
+    
+     export const printName3=(name,returnName)=> {
+    console.log("name is:",name)
+    returnName = name+"GUPTA"
+    return(returnName)
+    }
